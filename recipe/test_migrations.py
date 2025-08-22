4f3953c4fd26f4fc5ce8d21105853e382a1df591
beckermr <becker.mr@gmail.com>
2025-08-22 10:15:12 -0500

    refactor: use fixtures

--- recipe/test_migrations.py
+++ recipe/test_migrations.py
@@ -15,6 +15,79 @@ all_migration_ids = [os.path.basename(pth) for pth in all_migrations]
 print(f"Checking migrations in {migrations_path}", flush=True)
 
 
+@pytest.fixture(scope="session")
+def pr_all_uuids():
+    with open(
+        migrations_path / ".." / "migration_support" / "uuids.yaml",
+        "r",
+        encoding="utf-8",
+    ) as f:
+        all_uuids = frozenset(yaml.load(f, Loader=yaml.SafeLoader)["uuids"])
+    return all_uuids
+
+
+@pytest.fixture(scope="session")
+def main_all_uuids():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        subprocess.run(
+            [
+                "git",
+                "clone",
+                "--depth=1",
+                "https://github.com/conda-forge/conda-forge-pinning-feedstock.git",
+                os.path.join(tmpdir, "cfp"),
+            ],
+            check=True,
+            capture_output=True,
+        )
+        with open(
+            os.path.join(tmpdir, "cfp", "recipe", "migration_support", "uuids.yaml"),
+            "r",
+            encoding="utf-8",
+        ) as f:
+            all_uuids = yaml.load(f, Loader=yaml.SafeLoader)["uuids"]
+            assert len(set(all_uuids)) == len(all_uuids), (
+                "UUIDs in `recipe/migration_support/uuids.yaml` on main are not unique!"
+            )
+        return frozenset(all_uuids)
+
+
+@pytest.fixture(scope="session")
+def current_migrations():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        subprocess.run(
+            [
+                "git",
+                "clone",
+                "--depth=1",
+                "https://github.com/conda-forge/conda-forge-pinning-feedstock.git",
+                os.path.join(tmpdir, "cfp"),
+            ],
+            check=True,
+            capture_output=True,
+        )
+        return frozenset(
+            [
+                os.path.basename(pth)
+                for pth in glob.glob(
+                    os.path.join(tmpdir, "cfp", "recipe", "migrations", "*.yaml")
+                )
+            ]
+        )
+
+
+def test_pr_all_uuids_unique():
+    with open(
+        migrations_path / ".." / "migration_support" / "uuids.yaml",
+        "r",
+        encoding="utf-8",
+    ) as f:
+        all_uuids = yaml.load(f, Loader=yaml.SafeLoader)["uuids"]
+        assert len(set(all_uuids)) == len(all_uuids), (
+            "UUIDs in `recipe/migration_support/uuids.yaml` in pr are not unique!"
+        )
+
+
 def test_all_extensions_are_yaml():
     assert set(migrations_path.glob("*.yml")) == set()
 
@@ -55,74 +128,20 @@ def test_uuids_unique_in_pr():
 
 
 @pytest.mark.parametrize("filename", all_migrations, ids=all_migration_ids)
-def test_uuids_recorded_in_pr(filename):
-    with open(
-        migrations_path / ".." / "migration_support" / "uuids.yaml",
-        "r",
-        encoding="utf-8",
-    ) as f:
-        all_uuids = yaml.load(f, Loader=yaml.SafeLoader)["uuids"]
-        assert len(set(all_uuids)) == len(all_uuids), (
-            "UUIDs in `recipe/migration_support/uuids.yaml` in pr are not unique!"
-        )
-    all_uuids = frozenset(all_uuids)
-
+def test_uuids_recorded_in_pr(filename, pr_all_uuids):
     with open(filename, "r", encoding="utf-8") as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
-        assert data["__migrator"]["uuid"] in all_uuids, (
+        assert data["__migrator"]["uuid"] in pr_all_uuids, (
             f"Migrator {os.path.basename(filename)} does not have its "
             "UUID recorded in `recipe/migration_support/uuids.yaml`!"
         )
 
 
-def test_uuids_against_main():
-    with open(
-        migrations_path / ".." / "migration_support" / "uuids.yaml",
-        "r",
-        encoding="utf-8",
-    ) as f:
-        pr_all_uuids = yaml.load(f, Loader=yaml.SafeLoader)["uuids"]
-        assert len(set(pr_all_uuids)) == len(pr_all_uuids), (
-            "UUIDs in `recipe/migration_support/uuids.yaml` in pr are not unique!"
-        )
-    pr_all_uuids = frozenset(pr_all_uuids)
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        subprocess.run(
-            [
-                "git",
-                "clone",
-                "--depth=1",
-                "https://github.com/conda-forge/conda-forge-pinning-feedstock.git",
-                os.path.join(tmpdir, "cfp"),
-            ],
-            check=True,
-            capture_output=True,
-        )
-        with open(
-            os.path.join(tmpdir, "cfp", "recipe", "migration_support", "uuids.yaml"),
-            "r",
-            encoding="utf-8",
-        ) as f:
-            all_uuids = yaml.load(f, Loader=yaml.SafeLoader)["uuids"]
-            assert len(set(all_uuids)) == len(all_uuids), (
-                "UUIDs in `recipe/migration_support/uuids.yaml` on main are not unique!"
-            )
-        all_uuids = frozenset(all_uuids)
-
-        curr_migrations = set(
-            [
-                os.path.basename(pth)
-                for pth in glob.glob(
-                    os.path.join(tmpdir, "cfp", "recipe", "migrations", "*.yaml")
-                )
-            ]
-        )
-
-    assert len(pr_all_uuids) >= len(all_uuids), (
+def test_uuids_against_main(pr_all_uuids, main_all_uuids, current_migrations):
+    assert len(pr_all_uuids) >= len(main_all_uuids), (
         "Some UUIDs were removed from `migration_support/uuids.yaml` in the PR. Do not do that!"
     )
-    assert all_uuids <= pr_all_uuids, (
+    assert main_all_uuids <= pr_all_uuids, (
         "Some UUIDs were removed from `migration_support/uuids.yaml` in the PR. Do not do that!"
     )
 
@@ -131,16 +150,16 @@ def test_uuids_against_main():
     for filename in all_migrations:
         with open(filename, "r", encoding="utf-8") as f:
             data = yaml.load(f, Loader=yaml.SafeLoader)
-            if os.path.basename(filename) not in curr_migrations:
+            if os.path.basename(filename) not in current_migrations:
                 new_uuids.add(data["__migrator"]["uuid"])
             else:
                 old_uuids.add(data["__migrator"]["uuid"])
 
-    assert old_uuids <= all_uuids, (
+    assert old_uuids <= main_all_uuids, (
         "Some current migrator UUIDs are not recorded in the list of all "
         "UUIDs in `recipe/migration_support/uuids.yaml` on main!"
     )
-    assert new_uuids.isdisjoint(all_uuids), (
+    assert new_uuids.isdisjoint(main_all_uuids), (
         "Some new migrator UUIDs are already recorded in the list of all "
         "UUIDs in `recipe/migration_support/uuids.yaml` on main indicating a UUID collision!"
     )
